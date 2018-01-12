0d8c81da73fba728bfea50b30e5dcecf9fbbc67b
Ernest Bruno and Justin Lardani <ernest@>
2018-01-11 16:35:25 -0800

    Create Scripts for functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+  var explore = function(side1, side2, side3){
+    if (side1 === side2 && side2 === side3){
+      return "equilateral";
+    } else if (side1 !== side2 || side2 !== side3 || side1 !== side3) {
+      return "scalene";
+    }else{
+      return "isosceles";
+    };
+  };
 
 
+  $("#sides").submit(function(event) {
+    var side1 = $("#side1").val();
+    var side2 = $("#side2").val();
+    var side3 = $("#side3").val();
+    console.log(side1);
+    $(".typeTri").text(explore(side1, side2, side3));
+    $("h2").show();
+    event.preventDefault();
+  });
 });
